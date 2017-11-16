816639557be05dcde2e81402275a31e995b02df0
gwib <galinavj@stud.ntnu.no>
2017-11-16 16:49:09 +0100

    adding class for recipe and recipebase, way to go

---
+++ src/Recipe.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Recipe {
+	
+	public String name;
+	public ArrayList<FoodItem> ingredients;
+	public ArrayList<Double> quantities;
+	public String description;
+	
+	public Recipe(String n, ArrayList<FoodItem> ing, ArrayList<Double> q, String d) {
+		this.name = n;
+		this.ingredients = ing;
+		this.quantities = q;
+		this.description = d;
+	}
+	
+	//TODO: read recipes from csv and add these recipes to recipebase
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<FoodItem> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(ArrayList<FoodItem> ingredients) {
+		this.ingredients = ingredients;
+	}
+	public ArrayList<Double> getQuantities() {
+		return quantities;
+	}
+	public void setQuantities(ArrayList<Double> quantities) {
+		this.quantities = quantities;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
