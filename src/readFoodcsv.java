f3fb324e423f1560b8d1c15cf6a889a53a53c5e5
Zana <zanad@DESKTOP-HR2VDFM.home>
2017-11-27 10:43:50 +0100

    Changes class name from 'readFoodcv' to 'readFoodcsv'

--- src/readFoodcsv.java
+++ src/readFoodcsv.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class readFoodcv {
+public class readFoodcsv {
 
 	public static void main(String[] args) throws IOException{
 		      
